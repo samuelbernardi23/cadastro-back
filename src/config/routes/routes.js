5bc16c64751070acfe3d76f51a4267a390657a6d
Samuka <samuelbernardi22@gmail.com>
2019-10-10 21:51:26 -0300

    hello world

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -1,24 +1,5 @@
 module.exports = (app) => {
     app.get('/', (req, res) => {
-        res.marko(require('../../public/index.marko'), {
-            livros: [
-                {
-                    id: 1,
-                    titulo: "Mateus"
-                },
-                {
-                    id: 2,
-                    titulo: "Marcos"
-                },
-                {
-                    id: 2,
-                    titulo: "Lucas"
-                },
-                {
-                    id: 2,
-                    titulo: "João"
-                }
-            ]
-        })
+        res.marko(require('../../public/index.marko'))
     });
 }
