f5dcb601550ea09e43e0179baa3147a8e45c1381
Samuka <samuelbernardi22@gmail.com>
2019-10-14 17:15:02 -0300

    Inserindo o CORS para o front ter acesso a api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const app = require('./src/config/custom-express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 mongoose.connect('mongodb+srv://samuel:samuel@cluster0-nvvrf.mongodb.net/igreja?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.listen(process.env.PORT || 3000);
